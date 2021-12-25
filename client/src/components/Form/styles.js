c237def7dabb046d7d34c0296fc910b3c67861db
Ebuzer Yalcin <ebuzeryalcin@Macbook-m1.local>
2021-12-25 22:19:04 +0100

    my translation app initial commit

---
+++ client/src/components/Form/styles.js
@@ -0,0 +1,30 @@
+
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
+  form: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  fileInput: {
+    width: '97%',
+    margin: '10px 0',
+  },
+  buttonSubmit: {
+    marginBottom: 10,
+    backgroundColor: 'rgb(134, 59, 196);',
+  },
+  buttonClear:{
+    marginBottom: 10,
+    backgroundColor: 'rgb(255, 115, 0)',
+  }
+}));
---
+++ client/src/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    color: 'rgb(255, 115, 0)',
+  },
+  image: {
+    marginLeft: '15px',
+  },
+}));
